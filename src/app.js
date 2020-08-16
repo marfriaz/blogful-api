c7a83559b2952433c9efcc9d29f55da95429a50d
Marco Friaz <marfriaz@gmail.com>
2020-08-16 16:17:40 -0700

    Heroku server

--- src/app.js
+++ src/app.js
@@ -4,38 +4,23 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const ArticlesService = require("./articles-service");
+const articlesRouter = require("./articles/articles-router");
+const usersRouter = require("./users/users-router");
+const commentsRouter = require("./comments/comments-router");
 
 const app = express();
 
-const morganOption = NODE_ENV === "production";
-
-app.use(morgan(morganOption));
-app.use(helmet());
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  })
+);
 app.use(cors());
+app.use(helmet());
 
-app.get("/articles", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getAllArticles(knexInstance)
-    .then((articles) => {
-      res.json(articles);
-    })
-    .catch(next);
-});
-
-app.get("/articles/:article_id", (req, res, next) => {
-  const knexInstance = req.app.get("db");
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then((article) => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` },
-        });
-      }
-      res.json(article);
-    })
-    .catch(next);
-});
+app.use("/api/articles", articlesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/comments", commentsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
@@ -44,7 +29,7 @@ app.get("/", (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
+    response = { error: "Server error" };
   } else {
     console.error(error);
     response = { message: error.message, error };
